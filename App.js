c3c3a6f8e49723fcc3a297a7bdbcf6891434757f
Aman Bansod <93021401+Ammy076@users.noreply.github.com>
2024-08-15 08:59:37 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import JobsScreen from './src/screens/JobsScreen';
+import BookmarksScreen from './src/screens/BookmarksScreen';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Jobs" component={JobsScreen} />
+          <Tab.Screen name="Bookmarks" component={BookmarksScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
