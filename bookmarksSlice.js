db9e73cef12651d9e5b7b83fd095a257a358de8e
Aman Bansod <93021401+Ammy076@users.noreply.github.com>
2024-08-15 09:02:02 +0530

    Create bookmarksSlice.js

---
+++ bookmarksSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const bookmarksSlice = createSlice({
+  name: 'bookmarks',
+  initialState: [],
+  reducers: {
+    addBookmark: (state, action) => {
+      const updatedBookmarks = [...state, action.payload];
+      AsyncStorage.setItem('bookmarks', JSON.stringify(updatedBookmarks));
+      return updatedBookmarks;
+    },
+    setBookmarks: (state, action) => {
+      return action.payload;
+    }
+  }
+});
+
+export const { addBookmark, setBookmarks } = bookmarksSlice.actions;
+
+export const loadBookmarks = () => async (dispatch) => {
+  const bookmarks = await AsyncStorage.getItem('bookmarks');
+  if (bookmarks) {
+    dispatch(setBookmarks(JSON.parse(bookmarks)));
+  }
+};
+
+export default bookmarksSlice.reducer;
